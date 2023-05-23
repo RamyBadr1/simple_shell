a0cec89e39abbc512d3caa708c819a2b701ec88d
RamyBadr1 <ramybadr054@gmail.com>
2023-05-24 01:51:49 +0300

    first commit

---
+++ main.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stddef.h>
+#include <fcntl.h>
+
+/**
+ * info - informations
+ * @toke: token
+ * @env: enviroment
+ * @name: name
+ * @c_name: c_name
+ * Description: informations
+*/
+typedef struct info
+{
+	char **toke;
+	char **env;
+	char *name;
+	char *c_name;
+} data_p;
+
+/**
+ * builtins - builtin functions
+ * @env: env
+ * @exit: exit
+ * Description: builtin functions
+*/
+typedef struct builtin
+{
+	char *env;
+	char *exit;
+} builtin;
+
+void s_handler(int s);
+void set_data(data_p *data, char *agv[]);
+int _execute(data_p *data);
+void shell_loop(data_p *data, char *prompt);
+void _handler(int s);
+int get_locat(data_p *data);
+void free_data(data_p *data);
+void free_array(char **array);
+
+#endif
