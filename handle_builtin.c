e108a23398be740c245190afa19bf4587297b790
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-23 17:22:30 -0700

    handle built in command

---
+++ handle_builtin.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+
+/**
+* exit_shell - exit the shell
+* @data: a pointer to a struct array
+*/
+
+void exit_shell(data_p *data)
+{
+free_data(data);
+exit(0);
+}
+
+/**
+* print_env - print the enviromint variable
+*/
+
+void print_env(void)
+{
+int x = 0;
+char **env = environ;
+
+while (env[x])
+{
+write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
+write(STDOUT_FILENO, "\n", 1);
+x++;
+}
+}
+
+/**
+* handl_builtin - handle th builtin command
+* @data: a pointer to a struct array
+* Return: 0;
+*/
+
+int handl_builtin(data_p *data)
+{
+builtin built = {"env", "exit"};
+
+if (_strcmp(data->c_name, built.env) == 0)
+{
+print_env();
+return (-1);
+}
+else if (_strcmp(data->c_name, built.exit) == 0)
+{
+exit_shell(data);
+return (-1);
+}
+return (0);
+}
+
