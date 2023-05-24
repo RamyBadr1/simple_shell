3700b0579aa78289a761f38d27ea488252d33dcc
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-05-23 17:39:02 -0700

    add frees function

---
+++ free.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+* free_array - free an array of string
+* @array: pointer of string
+*/
+
+void free_array(char **array)
+{
+int i;
+
+if (array != NULL)
+{
+for (i = 0; array[i]; i++)
+free(array[i]);
+
+free(array);
+array = NULL;
+}
+}
+
+/**
+* free_data - free array of struct
+* @data: a pointer to a struct array
+*/
+
+void free_data(data_p *data)
+{
+if (data->toke)
+free_array(data->toke);
+if (data->c_name)
+free(data->c_name);
+
+data->c_name = NULL;
+data->toke = NULL;
+}
