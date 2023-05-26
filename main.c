ef8aced415dc731aeb116fecadd02dbf20065500
RamyBadr1 <ramybadr054@gmail.com>
2023-05-27 01:23:20 +0300

    main.c

--- main.c
+++ main.c
@@ -1,44 +1,44 @@
 #include "main.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
+ * main - main function
+ * @argc: argc length
+ * @argv: argv array
+ * Return: 0 on success || 1 on error
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	info_t information[] = { INFO_INIT };
+	int filedescriptor = 2;
 
 	asm ("mov %1, %0\n\t"
 			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+			: "=r" (filedescriptor)
+			: "r" (filedescriptor));
 
-	if (ac == 2)
+	if (argc == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		filedescriptor = open(argv[1], O_RDONLY);
+		if (filedescriptor < 0)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
+				_eputs(argv[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
+				_eputs(argv[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
+
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		information->readfd = filedescriptor;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
+	populate_env_list(information);
+	read_history(information);
+	hsh(information, argv);
 	return (EXIT_SUCCESS);
 }
